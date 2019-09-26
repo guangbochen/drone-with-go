7a43a38f768fd4e496023448ce68d1354c32a0c2
Guangbo Chen <steven.guangbo.chen@gmail.com>
2019-09-26 11:45:00 +0800

    update hello world2

--- main_test.go
+++ main_test.go
@@ -29,7 +29,7 @@ func TestHelloWorld(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `Hello New World!!`
+	expected := `Hello New World2!!`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
